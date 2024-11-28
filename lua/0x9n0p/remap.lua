b9b127f9222dce198a852441b46c1ccd09b1b70f
Mohammad Amin Ahmadi <0x9n0p@gmail.com>
2024-11-28 14:41:23 +0330

    Map 'qq' to close current buffer

--- lua/0x9n0p/remap.lua
+++ lua/0x9n0p/remap.lua
@@ -3,6 +3,12 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<Esc>")
 
+-- close current buffer.
+vim.keymap.set("n", "qq", function()
+	vim.cmd("q")
+end)
+
+-- write and close current buffer and back to the file navigator.
 vim.keymap.set("n", "wq", function()
 	vim.cmd("w")
 	vim.cmd.Ex()
